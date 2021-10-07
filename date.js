d646935b9832e1c3f1c9795d6ea72353e6f153a0
parksubon <88330287+parksubon@users.noreply.github.com>
2021-10-07 17:01:23 +0900

    Add files via upload

---
+++ date.js
@@ -0,0 +1,103 @@
+const qnaList = [
+  {
+    q: '1. Are you spontaneous <br> or planned for a trip?',
+    a: [
+      { type: [1, 2, 4, 9] },
+      { type: [0, 3, 6, 5, 10, 8] },
+    ]
+  },
+  {
+    q: '2. Which one do you want, <br> a mountain or a sea?',
+    a: [
+      { type: [0, 3, 2, 8] },
+      { type: [1, 6, 5, 10] },
+    ]
+  },
+  {
+    q: '3. Do you like staying <br> at a hotel or a pension?',
+    a: [
+      { type: [1, 3, 2, 10, 8] },
+      { type: [7, 9, 11] },
+    ]
+  },
+  {
+    q: '4. Overseas travel <br> or domestic travel?',
+    a: [
+      { type: [1, 2, 4 ] },
+      { type: [7, 10, 8, 9, 11 ] },
+    ]
+  },
+  {
+    q: '5. If you order food, <br> what menu will you choose?',
+    a: [
+      { type: [4, 9, 11 ] },
+      { type: [0, 3, 6, 10 ] },
+    ]
+  },
+  {
+    q: '6. Do you like taking pictures <br> or taking pictures in your eyes?',
+    a: [
+      { type: [1, 7, 11 ] },
+      { type: [2, 4, 9 ] },
+    ]
+  },
+  {
+    q: '7. Which season do you like <br> to travel, summer or winter?',
+    a: [
+      { type: [0, 4, 9 ] },
+      { type: [3, 2, 6, 5, 10, 8 ] },
+    ]
+  }
+]
+  
+  const infoList = [
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '오늘도 좋은 하루 보내!'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '나 스스로가 풍요로운 사람이 되려고 노력하자!'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '하루 계획표를 달성하여 성취감을 얻자!'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '늘 노력하는 사람이 되자!'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '신은 용기있는자를 결코 버리지 않는다'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '삶이 있는 한 희망은 있다'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '행복의 문이 하나 닫히면 다른 문이 열린다'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '피할수 없으면 즐겨라'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '절대 어제를 후회하지마라'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '한번의 실패와 영원한 실패를 혼동하지 마라'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '자존감은 하루 하루 <br> 성실함에서부터 시작된다 <br> 알겠지? 수빈아 열심히해! <br> 사랑해!'
+    },
+    {
+      name: 'LOVE It! <br> 자신을 깎아내리지 말며 <br> 나 자신을 항상 존귀한 사람으로 대하며 <br> 적극적인 삶을 살자!',
+      desc: '오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아간다'
+    },
+  ]
+  
